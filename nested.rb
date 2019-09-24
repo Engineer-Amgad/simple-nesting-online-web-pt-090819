ad3ae46db6b6504bda85add456468b3a73bf73ae
Amgad Malik <amgadmalik@yahoo.com>
2019-09-24 01:18:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-  programmer_hash[:]
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:]
 end
 
 def adding_matz
